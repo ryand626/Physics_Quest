2234fed1c720c05ae8ec7f645065c6f4384457ab
Ryan Dougherty <Ryan@resnet161-44.medford.tufts.edu>
2015-02-07 21:26:00 -0500

    added a gravitate script
    The script allows the player to have the ball on the screen gravitate
    towards, or away from their player avatar based on the polarity of the
    player.

---
+++ Assets/Gravitate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Gravitate : MonoBehaviour {
+	public float G;
+	public List<GameObject> Players;
+
+
+	void Start () {
+		G = 1f; // Gravitational Constant
+	}
+
+	void Update () {
+		// Recalculate the force on the ball
+		constantForce.force = Vector3.zero; // Get rid of the previous force calculations
+
+		// For every player, calculate the force applied to the ball
+		foreach (GameObject player in Players){
+			// Calculate the magnitude and direction
+			float magnitude = (G * player.rigidbody.mass * rigidbody.mass) / Vector3.SqrMagnitude(player.transform.position-gameObject.transform.position);
+			Vector3 direction = player.transform.position - gameObject.transform.position;
+
+			// Check the polarity and alter the magnitude accordingly
+			Polarity polarity = player.GetComponent<Polarity>(); 
+			if(!polarity.state){
+				magnitude *= -1;
+			}
+
+			// Add the force to the overall force
+			constantForce.force = constantForce.force + (magnitude * direction);
+		}
+	}
+}
---
+++ Assets/Scripts/Polarity.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Polarity : MonoBehaviour {
+	public bool state;       // True = Positive, False = Negative
+	public float strength;   // Degree to which the object repels/attracts
+
+
+	void Start () {
+		state = true;
+		strength = 1f;
+	}
+	
+	// Change the Polarity of the object
+	public void ChangePolarity(){
+		state = !state;
+	}
+
+	// Set Polarity of Object
+	public void SetPolarity(bool newPolarity){
+		state = newPolarity;
+	}
+
+	// Set the strength of the object
+	public void SetStrength(float newStrength){
+		strength = newStrength;
+	}
+}
