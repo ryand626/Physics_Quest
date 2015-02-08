840c756d371f392c6505e342318f785ad301b2b4
Ryan Dougherty <Ryan@resnet161-44.medford.tufts.edu>
2015-02-07 21:51:12 -0500

    changed gravitation to electrostatic force

---
+++ Assets/Scripts/ElectrostaticForce.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ElectrostaticForce : MonoBehaviour {
+	public float k;
+	public float q1;
+	public float q2;
+	public List<GameObject> Players;
+	
+	
+	void Start () {
+		k = 1f; // Constant
+		q1 = 1f;
+		q2 = 1f;
+	}
+	
+	void Update () {
+		// Recalculate the force on the ball
+		constantForce.force = Vector3.zero; // Get rid of the previous force calculations
+		
+		// For every player, calculate the force applied to the ball
+		foreach (GameObject player in Players){
+			// Check the polarity and alter the magnitude accordingly
+			Polarity polarity = player.GetComponent<Polarity>(); 
+			if(!polarity.state){
+				q2 = -1f;
+			}else{
+				q2 = 1f;
+			}
+			q2 *= polarity.strength;
+
+			// Calculate the magnitude and direction
+			float magnitude = (k * q1 * q2) / Vector3.SqrMagnitude(player.transform.position - gameObject.transform.position);
+			Vector3 direction = player.transform.position - gameObject.transform.position;
+
+			
+			// Add the force to the overall force
+			constantForce.force = constantForce.force + (magnitude * direction);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Physics_Quest
 An educational touch table boardgame that teaches physics principles
+
+##Games
+###Magnet Snake
+Teaches the basics of magnetic polarity
