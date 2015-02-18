d07b9fa01b6fef6044fcbaedb81dec29b05a3d8c
Ryan Dougherty <Ryan@GLaDOS-2.local>
2015-02-18 17:39:10 -0500

    ball works a little better
    In the future: should only move with the car when it is on the car

---
+++ Assets/CarBall.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarBall : MonoBehaviour {
+	public float mass; // mass of ball
+	public float mu; // frictional coefficient
+	public float velocity; // velocity of ball
+
+	public CarPhysics Car; // car the ball is on
+
+	public float slow_factor; // factor by which the ball slows down when it falls off the car
+
+	void Start(){
+		velocity = 0f;
+		slow_factor = .5f;
+	}
+
+	// Keep the ball on the car if there is enough frictional force
+	void Update () {
+//		print ("FRICTION: " + mass * Physics.gravity.y * mu);
+//		print("CAR: " + Car.mass * Car.acceleration);
+
+		// Have the ball slow down when it falls off
+		if (velocity > 0) {
+			velocity -= slow_factor;
+		}
+		if (velocity < 0) {
+			velocity += slow_factor;
+		}
+		// Need to check if the ball is on the car
+		if(Car.transform.GetChild(0).renderer.bounds.Intersects(renderer.bounds)){//this doesn't work, should just make a rectangle, or do ray casting
+			print("IN BOUNDS");
+			if (mass * (Physics.gravity.y * -1f) * mu > Car.mass * Car.acceleration) {
+				velocity = Car.velocity;
+			}
+		}
+		transform.Translate(velocity * Time.deltaTime,0f,0f);
+	}
+}
--- Assets/Scripts/Car/PlayerControls.cs
+++ Assets/Scripts/Car/PlayerControls.cs
@@ -2,12 +2,16 @@
 using System.Collections;
 
 public class PlayerControls : MonoBehaviour {
+	// Objects
 	public RoadController Road;
+	public CarPhysics Car;
 
+	// Constants
 	public float acceleration_factor;
 	public float brake_factor;
 	public float top_speed;
 
+	// Variables
 	public float playerSpeed;
 	private float playerDirection;
 	
@@ -22,7 +26,7 @@ public class PlayerControls : MonoBehaviour {
 	// Move the car according to the road speed, and the car speed
 	void Update () {
 		//Road.backgroundSpeed * Road.backgroundDirection * .2f +
-		offset =  playerSpeed * playerDirection * Road.backgroundDirection;
+		offset =  playerSpeed * playerDirection * Road.backgroundDirection; // player direction doesn't do anything atm, so i'm using the road direction as a placeholder
 		transform.Translate (offset * Time.deltaTime, 0f, 0f);
 
 		if (Input.GetKey (KeyCode.A)) {
@@ -31,12 +35,18 @@ public class PlayerControls : MonoBehaviour {
 		if (Input.GetKey (KeyCode.B)) {
 			Brake();
 		}
+		if (!Input.anyKey) {
+			Car.acceleration = 0f;
+		}
+		// Update the velocity of the car to be used by the ball script
+		Car.velocity = playerSpeed * playerDirection * Road.backgroundDirection; // same here
 	}
 
 	// Accelerate the car up to the top_speed
 	public void Accelerate(){
 		if(playerSpeed + acceleration_factor < top_speed){
 			playerSpeed += acceleration_factor;
+			Car.acceleration = acceleration_factor;
 		}else{
 			playerSpeed = top_speed;
 		}
@@ -46,6 +56,7 @@ public class PlayerControls : MonoBehaviour {
 	public void Brake(){
 		if(playerSpeed - brake_factor > 0f){
 			playerSpeed -= brake_factor;
+			Car.acceleration = brake_factor * -1f;
 		}else{
 			playerSpeed = 0f;
 		}
