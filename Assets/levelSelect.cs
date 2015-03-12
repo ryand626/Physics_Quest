49b8161da863796b1327ee35e5af7b25b9a3a241
Ryan Dougherty <ryand626@gmail.com>
2015-03-12 17:01:58 -0400

    Level select
    made a level select for the first 3 levels….it is very basic looking

---
+++ Assets/levelSelect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray mouseRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (mouseRay, out hit, 300)) {
+			if(hit.collider.name == "car"){
+				print("CAR");
+				if(Input.GetMouseButtonDown(0)){
+					Application.LoadLevel (1);
+				}
+			}
+			if(hit.collider.name == "magnet"){
+				print("MAGNET");
+				if(Input.GetMouseButtonDown(0)){
+					Application.LoadLevel (0);
+				}
+			}
+			if(hit.collider.name == "mirror"){
+				print("MIRROR");
+				if(Input.GetMouseButtonDown(0)){
+					Application.LoadLevel (2);
+				}
+			}
+		}
+	}
+}
