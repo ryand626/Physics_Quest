cc33b9e6ed162561f08445e94d7f5f5e04499151
Ryan Dougherty <ryand626@gmail.com>
2015-03-22 16:02:01 -0400

    Add and rotate mirrors working
    Each player can tap their tray to spawn a mirror, and then rotate the
    mirror on the tray to rotate their mirror on the board

--- Assets/Scripts/Mirror/AddMirror.cs
+++ Assets/Scripts/Mirror/AddMirror.cs
@@ -9,70 +9,71 @@ public class AddMirror : MonoBehaviour {
 	public GameObject uitray;
 	public Sprite mirrorSprite;
 	private bool mirrorExists;
-	// Use this for initialization
-	void Start () {
-		////GetComponent<TapGesture>().StateChanged += tappedHandler;
-		mirrorExists = false;
-		//GetComponent<PanGesture>().StateChanged += panHandler;
-		//GetComponent<RotateGesture>().StateChanged += rotateHandler;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-	private void tappedHandler(object sender, EventArgs e) {
-		print ("You tapped " + gameObject.name);
-		print (mirrorExists);
-		if (!mirrorExists) {
-			mirrorExists = true;
-			GameObject newMirror = (GameObject)Instantiate(Resources.Load("Mirror/Mirror"));
-			//newMirror.AddComponent ("Rigidbody");
-			////newMirror.AddComponent<Rigidbody>();
-			//newMirror.AddComponent ("BoxCollider");
-			////newMirror.AddComponent<BoxCollider>();
-			//newMirror.AddComponent ("SpriteRenderer");
-			////newMirror.AddComponent<SpriteRenderer>();
-			//newMirror.AddComponent("Rotate Gesture");
-			//newMirror.AddComponent("Pan Gesture");
-			SpriteRenderer s = newMirror.GetComponent<SpriteRenderer> ();
-			////newMirror.rigidbody.useGravity = false;
-			////newMirror.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
-			s.sprite = mirrorSprite;
-			////newMirror.transform.localScale = new Vector3 (1.5f, 1f, 1f);
-			////BoxCollider b = newMirror.GetComponent<BoxCollider> ();
-			////b.size = new Vector3 (1.1f, 0.15f, 0.2f);
-			if(uitray.name == "UITray Pink") {
-				newMirror.transform.position = new Vector3 (-4.7f, -2f, 0);
-			}
-			else if(uitray.name == "UITray Yellow") {
-				newMirror.transform.position = new Vector3 (3f, 4f, 0);
-			}
-			else if(uitray.name == "UITray Purple") {
-				newMirror.transform.position = new Vector3 (5f, 1.5f, 0);
-				newMirror.transform.Rotate (0,0,90f);
-			}
-			else {
-				newMirror.transform.position = new Vector3 (-4.7f, 4f, 0);
-			}
+	public GameObject mirror;
+	
+	void Start () {
+		if (!uitray) {
+			uitray = transform.parent.gameObject;
 		}
+		mirrorExists = false;
 	}
 
 	private void OnEnable()
 	{
 		GetComponent<TapGesture> ().Tapped += tappedHandler;
+		GetComponent<RotateGesture> ().Rotated += rotateHandler;
 	}
 	private void OnDisable()
 	{
 		if (this.enabled) {
 			GetComponent<TapGesture> ().Tapped -= tappedHandler;
+			GetComponent<RotateGesture> ().Rotated -= rotateHandler;
 		}
 	}
-//
-//	private void panHandler(object sender, GestureStateChangeEventArgs gestureStateChangeEventArgs) {
-//	}
-//
-//	private void rotateHandler(object sender, GestureStateChangeEventArgs gestureStateChangeEventArgs) {
-//	}
+
+	private void tappedHandler(object sender, EventArgs e) {
+		if (!mirrorExists) {
+			mirrorExists = true;
+			GameObject newMirror = (GameObject)Instantiate(Resources.Load("Mirror/Mirror"));
+
+			// Update the sprite depending on the player's mirror sprite
+			SpriteRenderer s = newMirror.GetComponent<SpriteRenderer> ();
+			s.sprite = mirrorSprite;
+
+			// Set the position and names
+			if(uitray.name == "UITray Pink") {
+				newMirror.transform.position = new Vector3 (-4.7f, -2f, 0);
+				newMirror.name = "Pink Mirror";
+			}
+			else if(uitray.name == "UITray Yellow") {
+				newMirror.transform.position = new Vector3 (3f, 4f, 0);
+				newMirror.name = "Yellow Mirror";
+			}
+			else if(uitray.name == "UITray Purple") {
+				newMirror.transform.position = new Vector3 (5f, 1.5f, 0);
+				newMirror.name = "Purple Mirror";
+			}
+			else {
+				newMirror.transform.position = new Vector3 (-4.7f, 4f, 0);
+				newMirror.name = "Orange Mirror";
+			}
+
+			newMirror.transform.rotation = transform.rotation;
+			mirror = newMirror;
+		}
+	}
+
+
+
+	private void rotateHandler(object sender, EventArgs e) {
+		RotateGesture rotation = (RotateGesture)sender;
+		RectTransform bounds = GetComponent<RectTransform> ();
+		if (bounds && mirror) {
+			transform.Rotate(0,0,rotation.DeltaRotation);//bounds.Rotate(0, 0, rotation.DeltaRotation);
+			mirror.transform.Rotate(0,0,rotation.DeltaRotation);
+		} else {
+			print ("No Rect Transform!");
+		}
+	}
 }
